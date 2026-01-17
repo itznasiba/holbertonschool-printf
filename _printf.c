3b75110dfd71b8d146024b7a1fd8148050b5f1b6
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-17 18:29:49 +0000

    1

---
+++ _printf.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf - produces output
+ * @format: format string
+ *
+ * Return: the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0;
+	int count = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args,format);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			write(1,&format[i],1);
+			count++;
+		}
+		else
+		{
+			i++;
+			if (format[i] =='c')
+				count+=print_char(args);
+			else if (format[i] == 's')
+				count+=print_string(args);
+			else if (format[i] =='%')
+				count+=print_percent();
+			else
+			{
+				write(1,'%',1);
+				write(1,&format[i],1);
+				count+=2;
+			}
+		}
+		i++;
+	}
+	va_end(args);
+	return(count);
+
+}
