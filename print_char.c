cff675d9069bce18a41f1c5ad8774d719cc85957
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-17 18:58:12 +0000

    1

---
+++ print_char.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * print_char - prints a character
+ * @args: arguments
+ *
+ * Return: number of printed elements
+ */
+
+int print_char(va_list args)
+{
+	char c;
+
+	c = va_arg(args, int);
+	write(1, &c, 1);
+	return (1);
+}
