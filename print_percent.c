68d1b6f06a3c039393e65fae0cfe94f92ac8ef08
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-17 19:20:21 +0000

    1

---
+++ print_percent.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * print_percent - prints percent
+ *
+ * Return: int
+ */
+
+int print_percent(void)
+{
+	write(1, "%", 1);
+	return (1);
+}
