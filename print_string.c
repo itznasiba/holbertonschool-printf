ecb954262ffc3d76d6753c7034e2466a511cd758
itznasiba <zeynallinesibe2006@gmail.com>
2026-01-17 19:13:20 +0000

    1

---
+++ print_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_string - prints a string
+ * @arg: arguments
+ *
+ * Return: the number of elements printed
+ */
+
+int print_string(va_list args)
+{
+	char *s;
+	int len = 0;
+
+	s = va_arg(args, char*);
+	if (!s)
+		s = "(null)";
+
+	while (*s)
+	{
+		write(1, &s, 1);
+		s++;
+		len++;
+	}
+	return (len);
+}
